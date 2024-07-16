feb0f9f6291c7fd143668f5b0e7693e75a6e5560
Muneeba2001 <muneebawaseem1006@gmail.com>
2024-07-16 20:52:59 +0500

    Admin Panel attched with Backend DB

--- src/Routes/FoodRoute/index.js
+++ src/Routes/FoodRoute/index.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
 
 const uploads = multer({ storage: storage });
 
-FoodRouter.get("/Food", FoodController.getAll);
+FoodRouter.get("/Food/List", FoodController.getAll);
 FoodRouter.get("/Food/:id", FoodController.getSingle);
 FoodRouter.post("/Food/add", uploads.single("Image"), FoodController.create);
 // FoodRouter.put("/Food/:id", FoodController.update);
